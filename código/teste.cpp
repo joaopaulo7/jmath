f878ebfc89729c7597db1f50b22af77268fe41fa
joaopaulo7 <joaopauloff7@hotmail.com>
2021-02-18 19:49:02 -0300

    Arquivo de deste adicionado para facilitar a visualização.

---
+++ código/teste.cpp
@@ -0,0 +1,42 @@
+/*
+ * teste.cpp
+ * 
+ * Copyright 2020 João Paulo Paiva Lima <joao.lima1@estudante.ufla.br>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+#include "jomath.hpp"
+
+int main()
+{
+    numComplexo tst;
+    tst.real = 1;
+    tst.imag = 1.4;
+    printf("%.14f\n", cos(getPi()+1));
+    printf("%.14f\n", arccos(cos(getPi()+1)));
+    
+    tst = exp(tst);
+    printf("%.14f + %.14fi\n", tst.real, tst.imag);
+    tst = ln(tst);
+    printf("%.14f + %.14fi\n", tst.real, tst.imag);
+    
+}
+
